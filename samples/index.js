cc16ea5dfcb70a22347dd4da277bbef1ce4a6162
KateNegrienko <katya@negrienko.com>
2016-10-19 13:13:47 +0300

    update themes

--- samples/index.js
+++ samples/index.js
@@ -43,7 +43,8 @@
     // Configure application services before start
     thisModule.config(
         function ($stateProvider, $urlRouterProvider, $mdIconProvider,
-                  $compileProvider, $httpProvider) { // pipTranslateProvider, pipSideNavProvider, pipAppBarProvider,
+                  $compileProvider, $httpProvider) { 
+            // pipTranslateProvider, pipSideNavProvider, pipAppBarProvider,
 
             $compileProvider.debugInfoEnabled(false);
             $httpProvider.useApplyAsync(true);
@@ -63,23 +64,22 @@
 
     thisModule.controller('pipSampleController',
 
-        function ($scope, $rootScope, $injector, $state, $mdSidenav, $timeout, $mdTheming, $mdMedia, localStorageService) {
+        function ($scope, $rootScope, $injector, $state, $mdSidenav, $timeout, $mdTheming,
+                  $mdMedia, localStorageService) {
 
             var pipTranslate = $injector.has('pipTranslate') ? $injector.get('pipTranslate') : null,
-                // appThemesDefault = $injector.has('appThemesDefault') ? $injector.get('appThemesDefault') : null,
                 pipTheme = $injector.has('pipTheme') ? $injector.get('pipTheme') : null;
 
             $scope.isTranslated = !!pipTranslate;
             $scope.isTheme = !!pipTheme;
             $scope.$mdMedia = $mdMedia;
 
-            $rootScope.$theme = localStorageService.get('theme');
+            $rootScope.$theme = localStorageService.get('theme') || 'blue';
             if ($scope.isTheme) {
                 $scope.themes = _.keys(_.omit($mdTheming.THEMES, 'default'));
             } else {
                 $scope.themes = [];
             }
-            
 
             $scope.languages = ['en', 'ru'];
             if (!$rootScope.$language) {
@@ -139,125 +139,3 @@
     );
 
 })(window.angular);
-
-
-// (function (angular) {
-//     'use strict';
-
-//     var thisModule = angular.module('appButtons',
-//         [
-//             'ngMaterial',
-//             'pipServices', 'pipButtons', //'appCoreServices.Toasts', 
-//             'pipLayout', 'pipNav', // 'pipDateTimes',
-//             'pipTheme.Default', 'pipTheme.Bootbarn', 'pipTheme',
-
-//             'appButtons.Refresh', 'appButtons.ToggleButtons'
-//         ]
-//     );
-
-//     // Configure application services before start
-//     thisModule.config(
-//         function ($stateProvider, $urlRouterProvider, pipTranslateProvider,
-//                    pipSideNavProvider, pipAppBarProvider, $mdIconProvider,
-//                   $compileProvider, $httpProvider) {
-
-//             $compileProvider.debugInfoEnabled(false);
-//             $httpProvider.useApplyAsync(true);
-            
-//             var content = [
-//                 { title: 'Refresh', state: 'refresh', url: '/refresh', auth: false,
-//                     controller: 'RefreshController', templateUrl: 'refresh_sample/refresh.html' },
-//                 { title: 'Toggle Buttons', state: 'toggle_buttons', url: '/toggle_buttons', auth: false,
-//                     controller: 'ToggleButtonsController', templateUrl: 'toggle_buttons_sample/toggle_buttons.html' },
-//             ],
-//             contentItem, i;
-
-//             $mdIconProvider.iconSet('icons', 'images/icons.svg', 512);
-
-//             pipAppBarProvider.globalSecondaryActions([
-//                 {name: 'global.signout', title: 'SIGNOUT', state: 'signout'}
-//             ]);
-
-//             // String translations
-//             pipTranslateProvider.translations('en', {
-//                 CONTROLS: 'Buttons',
-//                 SIGNOUT: 'Sign out'
-//             });
-
-//             pipTranslateProvider.translations('ru', {
-//                 CONTROLS: 'Кнопки',
-//                 SIGNOUT: 'Выйти'
-//             });
-
-//             for (i = 0; i < content.length; i++) {
-//                 contentItem = content[i];
-//                 $stateProvider.state(contentItem.state, contentItem);
-//             }
-
-//             $urlRouterProvider.otherwise('/progress');
-
-//             // Configure navigation menu
-//             pipSideNavProvider.sections([
-//                 {
-//                     links: [{title: 'CONTROLS', url: '/progress'}]
-//                 }/*, Links only for publishing samples
-//                 {
-//                     links: links
-//                 }
-
-//                 /*,
-//                 {
-//                     links: [{title: 'SIGNOUT', url: '/signout'}]
-//                 }*/
-//             ]);
-//         }
-//     );
-
-//     thisModule.controller('pipSampleController',
-//         function ($scope, $rootScope, $state, $mdSidenav, $timeout, pipTranslate, $mdTheming, pipTheme, 
-//                   $mdMedia) {
-
-//            // pipTheme.setCurrentTheme('bootbarn-warm');
-            
-//             $scope.pages = [
-//                 { title: 'Refresh', state: 'refresh', url: '/refresh',
-//                     controller: 'RefreshController', templateUrl: '../samples/refresh/refresh.html' },
-//                 { title: 'Toggle Buttons', state: 'toggle_buttons', url: '/toggle_buttons',
-//                     controller: 'ToggleButtonsController', 
-//                     templateUrl: '../samples/toggle_buttons/toggle_buttons.html' }
-//             ];
-
-//             var allThemes = _.keys(_.omit($mdTheming.THEMES, 'default'));
-//             $scope.themes = [];
-//             _.each(allThemes, function (theme) {
-//                 if (theme.indexOf('bootbarn') == -1) {
-//                     $scope.themes.push(theme);
-//                 }
-//             })
-
-//             $scope.selected = {};
-//             $timeout(function () {
-//                 $scope.selected.pageIndex = _.findIndex($scope.pages, {state: $state.current.name});
-//             });
-
-//             $scope.onNavigationSelect = function (stateName) {
-//                 if ($state.current.name !== stateName) {
-//                     $state.go(stateName);
-//                 }
-//             };
-
-//             $scope.onDropdownSelect = function (obj) {
-//                 if ($state.current.name !== obj.state) {
-//                     $state.go(obj.state);
-//                 }
-//             };
-
-//             $scope.isPadding = function () {
-//                 return $rootScope.$state
-//                     ? !($rootScope.$state.name === 'tabs' ||
-//                     $rootScope.$state.name === 'dropdown' && $mdMedia('xs')) : true;
-//             };
-//         }
-//     );
-
-// })(window.angular);
