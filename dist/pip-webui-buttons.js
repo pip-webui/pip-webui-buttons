3fd5c742a1c8129297806fb26bb427c4ad5dfbfa
Anastas Fonotov <anastasf@gmail.com>
2016-10-26 13:10:33 +0300

    Updated submodules

--- dist/pip-webui-buttons.js
+++ dist/pip-webui-buttons.js
@@ -30,6 +30,7 @@ module.run(['$templateCache', function($templateCache) {
 }]);
 })();
 
+/// <reference path="../typings/tsd.d.ts" />
 (function () {
     'use strict';
     angular.module('pipButtons', [
@@ -39,6 +40,7 @@ module.run(['$templateCache', function($templateCache) {
     ]);
 })();
 
+/// <reference path="../../typings/tsd.d.ts" />
 (function () {
     'use strict';
     var thisModule = angular.module('pipButtons.Translate', []);
@@ -51,6 +53,7 @@ module.run(['$templateCache', function($templateCache) {
     }]);
 })();
 
+/// <reference path="../../typings/tsd.d.ts" />
 (function () {
     'use strict';
     var thisModule = angular.module("pipFabTooltipVisibility", []);
@@ -75,50 +78,7 @@ module.run(['$templateCache', function($templateCache) {
     }]);
 })();
 
-(function () {
-    'use strict';
-    var thisModule = angular.module('pipRefreshButton', ['ngMaterial']);
-    thisModule.directive('pipRefreshButton', ['$parse', function ($parse) {
-        return {
-            restrict: 'EA',
-            scope: false,
-            template: String() +
-                '<md-button class="pip-refresh-button" tabindex="-1" ng-click="onClick($event)" aria-label="REFRESH">' +
-                '<md-icon md-svg-icon="icons:refresh"></md-icon>' +
-                '<span class="pip-refresh-text"></span>' +
-                '</md-button>',
-            replace: false,
-            link: function ($scope, $element, $attrs) {
-                var width, text, show, textGetter = $parse($attrs.pipText), visibleGetter = $parse($attrs.pipVisible), refreshGetter = $parse($attrs.pipRefresh), $button = $element.children('.md-button'), $text = $button.children('.pip-refresh-text');
-                show = function () {
-                    text = textGetter($scope);
-                    $text.text(text);
-                    $button.show();
-                    width = $button.width();
-                    $button.css('margin-left', '-' + width / 2 + 'px');
-                };
-                function hide() {
-                    $button.hide();
-                }
-                $scope.onClick = function () {
-                    refreshGetter($scope);
-                };
-                $scope.$watch(visibleGetter, function (newValue) {
-                    if (newValue) {
-                        show();
-                    }
-                    else {
-                        hide();
-                    }
-                });
-                $scope.$watch(textGetter, function (newValue) {
-                    $text.text(newValue);
-                });
-            }
-        };
-    }]);
-})();
-
+/// <reference path="../../typings/tsd.d.ts" />
 (function () {
     'use strict';
     var thisModule = angular.module('pipToggleButtons', ['pipButtons.Templates']);
@@ -177,6 +137,54 @@ module.run(['$templateCache', function($templateCache) {
             }
         };
     });
+})();
+
+/// <reference path="../../typings/tsd.d.ts" />
+(function () {
+    'use strict';
+    var thisModule = angular.module('pipRefreshButton', ['ngMaterial']);
+    thisModule.directive('pipRefreshButton', ['$parse', function ($parse) {
+        return {
+            restrict: 'EA',
+            scope: false,
+            template: String() +
+                '<md-button class="pip-refresh-button" tabindex="-1" ng-click="onClick($event)" aria-label="REFRESH">' +
+                '<md-icon md-svg-icon="icons:refresh"></md-icon>' +
+                '<span class="pip-refresh-text"></span>' +
+                '</md-button>',
+            replace: false,
+            link: function ($scope, $element, $attrs) {
+                var width, text, show, textGetter = $parse($attrs.pipText), visibleGetter = $parse($attrs.pipVisible), refreshGetter = $parse($attrs.pipRefresh), $button = $element.children('.md-button'), $text = $button.children('.pip-refresh-text');
+                show = function () {
+                    // Set a new text
+                    text = textGetter($scope);
+                    $text.text(text);
+                    // Show button
+                    $button.show();
+                    // Adjust position
+                    width = $button.width();
+                    $button.css('margin-left', '-' + width / 2 + 'px');
+                };
+                function hide() {
+                    $button.hide();
+                }
+                $scope.onClick = function () {
+                    refreshGetter($scope);
+                };
+                $scope.$watch(visibleGetter, function (newValue) {
+                    if (newValue) {
+                        show();
+                    }
+                    else {
+                        hide();
+                    }
+                });
+                $scope.$watch(textGetter, function (newValue) {
+                    $text.text(newValue);
+                });
+            }
+        };
+    }]);
 })();
 
 
