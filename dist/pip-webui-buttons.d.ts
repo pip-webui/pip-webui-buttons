e5b250849a469bc0dd738eebbdcdcaa8b2809c4f
KateNegrienko <katya@negrienko.com>
2017-01-13 15:18:40 +0200

    Start changing toggle buttons for typescript

--- dist/pip-webui-buttons.d.ts
+++ dist/pip-webui-buttons.d.ts
@@ -3,6 +3,26 @@ declare module pip.buttons {
 
 
 
+class ToggleButtonsController {
+    private _element;
+    private _scope;
+    private _timeout;
+    $mdMedia: angular.material.IMedia;
+    class: string;
+    multiselect: boolean;
+    buttons: any;
+    currentButtonValue: any;
+    currentButtonIndex: number;
+    currentButton: any;
+    buttonSelected: any;
+    disabled: any;
+    enterSpacePress: any;
+    ngDisabled: Function;
+    highlightButton: any;
+    change: Function;
+    onlyToggle: boolean;
+    constructor($mdMedia: angular.material.IMedia, $element: any, $attrs: angular.IAttributes, $scope: angular.IScope, $timeout: ng.ITimeoutService);
+}
 
 }
 
