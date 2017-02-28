f76a6107f1a7a2066b07af0903c146bff55d44d9
dvoikinalex <94dvoikin@gmail.com>
2017-02-28 18:19:12 +0300

    Updated submodules

--- dist/pip-webui-buttons.d.ts
+++ dist/pip-webui-buttons.d.ts
@@ -1,12 +1,12 @@
 declare module pip.buttons {
 
-
 class FabTooltipVisibilityController {
     private _element;
     private _scope;
     private _timeout;
     constructor($mdMedia: angular.material.IMedia, $element: any, $attrs: angular.IAttributes, $scope: angular.IScope, $timeout: ng.ITimeoutService, $parse: any);
 }
+
 
 
 
